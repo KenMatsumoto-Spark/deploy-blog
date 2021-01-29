4258ef57d6ccaf746d45e99d7a6cc1b9bc0083e3
KenMatsumoto-Spark <h264matsumoto@gmail.com>
2021-01-28 22:17:46 -0300

    added database url 2

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
